af9280c3a90407e2f753d6067143c4237c1db0c1
Kerbl4 <drallo.kir@gmail.com>
2022-12-15 22:47:18 +0400

    Added some new classes

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+
 class Board:
     """The sea battle game board."""
     def _init_(self):
@@ -8,6 +10,12 @@ class Board:
         """Creates a board canvas"""
         board_table = [["0" for i in range(6)] for i in range(6)]
         return board_table
+    
+    @property
+    def ships_list(self):
+        """List of the ships on the table"""
+        ships_list = []
+        return ships_list
     
     def print_board_table(self):
         """Prints the current state of a board table."""
@@ -23,9 +31,23 @@ class Board:
 
 class Dot:
     """A dot a.k.a. cell in our game board."""
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(self, i, j):
+        self.x = i
+        self.y = j
+    
+    def __eq__(self, other):
+        return self.i == other.i and self.j == other.j
+
+    def __str__(self):
+        return f"Dot:{self.i, self.j}"
+
+class Ship:
+    """The ship entity"""
+    def __init__(self, length, bow_point, direction, hp):
+        self.length = length
+        self.bow_point = bow_point
+        self.direction = direction
+        self.hp = hp
 
 
 sea_battle_board = Board()
